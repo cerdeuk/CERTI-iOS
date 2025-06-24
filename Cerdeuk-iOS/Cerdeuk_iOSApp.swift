b017403c4641f0411b097cb9f3156b03b7c8b615
OneTen19 <khy000426@naver.com>
2025-06-24 21:26:33 +0900

    Initial Commit

---
+++ Cerdeuk-iOS/Cerdeuk_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cerdeuk_iOSApp.swift
+//  Cerdeuk-iOS
+//
+//  Created by OneTen on 6/24/25.
+//
+
+import SwiftUI
+
+@main
+struct Cerdeuk_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
