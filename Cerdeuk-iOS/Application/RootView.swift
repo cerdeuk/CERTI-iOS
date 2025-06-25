dacf9a4462502fdfb8afd540a67c8f6a24965455
OneTen19 <khy000426@naver.com>
2025-06-25 17:30:27 +0900

    [Feat] #5 - 각 폴더별 view 추가 및 파일명 변경 - CD는 CerDeuk의 약자로 추후 변경예정

---
+++ Cerdeuk-iOS/Application/RootView.swift
@@ -0,0 +1,18 @@
+//
+//  RootView.swift
+//  Cerdeuk-iOS
+//
+//  Created by OneTen on 6/24/25.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        CDTabBarView()
+    }
+}
+
+#Preview {
+    RootView()
+}
---
+++ Cerdeuk-iOS/Presentation/CertificateDetail/CertificateDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  CertificateDetailView.swift
+//  Cerdeuk-iOS
+//
+//  Created by OneTen on 6/25/25.
+//
+
+import SwiftUI
+
+struct CertificateDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CertificateDetailView()
+}
