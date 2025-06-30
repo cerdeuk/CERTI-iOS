a198bcb3072758484a9c40f94a42fb6d8b9e6569
OneTen19 <khy000426@naver.com>
2025-07-01 01:16:12 +0900

    [Feat] #8 - BaseTargetType

---
+++ CERTI-iOS/Network/Base/BaseTargetType.swift
@@ -0,0 +1,43 @@
+//
+//  BaseTargetType.swift
+//  CERTI-iOS
+//
+//  Created by OneTen on 7/1/25.
+//
+
+import Foundation
+
+import Moya
+
+enum HeaderType {
+    case noneHeader
+    //    case accessTokenHeader
+    //    case refreshTokenHeader
+}
+
+protocol BaseTargetType: TargetType {
+    var headerType: HeaderType { get }
+}
+
+extension BaseTargetType {
+    
+    var baseURL: URL {
+        guard let urlString = Bundle.main.infoDictionary?["BASE_URL"] as? String,
+              let url = URL(string: urlString) else {
+            fatalError("🚨Base URL을 찾을 수 없습니다🚨")
+        }
+        return url
+    }
+    
+    var headers: [String: String]? {
+        var headers: [String: String] = [
+            "Content-Type": "application/json"
+        ]
+        
+        switch headerType {
+        case .noneHeader:
+            return nil
+        }
+    }
+    
+}
