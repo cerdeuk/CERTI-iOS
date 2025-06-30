7b1df7ff1846eba7c2d68f2858f40bfd206a2b79
OneTen19 <khy000426@naver.com>
2025-07-01 01:10:35 +0900

    [Feat] #8 - BaseResponseDTO

---
+++ CERTI-iOS/Network/Base/BaseResponseDTO.swift
@@ -0,0 +1,14 @@
+//
+//  BaseResponseDTO.swift
+//  CERTI-iOS
+//
+//  Created by OneTen on 7/1/25.
+//
+
+import Foundation
+
+struct BaseResponseDTO<T: Decodable>: Decodable {
+    let status: Int
+    let message: String
+    let data: T
+}
