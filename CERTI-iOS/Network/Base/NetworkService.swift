f4faece1b65ea8470c0ab45e49c7b335fee7c6c1
OneTen19 <khy000426@naver.com>
2025-07-01 01:17:55 +0900

    [Feat] #8 - NetworkService

---
+++ CERTI-iOS/Network/Base/NetworkService.swift
@@ -0,0 +1,21 @@
+//
+//  NetworkService.swift
+//  CERTI-iOS
+//
+//  Created by OneTen on 7/1/25.
+//
+
+import Foundation
+
+final class NetworkService {
+    
+    static let shared = NetworkService()
+    
+    private init() { }
+    
+//    이런식으로 서비스들 모아서 이 곳에서 선언 후 네트워크 서비스라는 객체를 하나만 생성해서 싱글톤 패턴으로 사용
+//    let authService: AuthServiceProtocol = AuthService()
+//    let authService: AuthServiceProtocol = AuthService()
+//    let authService: AuthServiceProtocol = AuthService()
+    
+}
