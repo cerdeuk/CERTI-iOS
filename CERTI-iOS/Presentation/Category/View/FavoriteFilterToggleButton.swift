f0fcb3b284a7b7ad396ea182224660589aa0e0a4
Yeonnies <eliyeon16@gmail.com>
2025-07-09 02:53:26 +0900

    [Feat] #24 - 즐겨찾기 버튼

---
+++ CERTI-iOS/Presentation/Category/View/FavoriteFilterToggleButton.swift
@@ -0,0 +1,36 @@
+//
+//  FavoriteFilterToggleButton.swift
+//  CERTI-iOS
+//
+//  Created by 김나연 on 7/9/25.
+//
+
+import SwiftUI
+
+struct FavoriteFilterToggleButton: View {
+    
+    let isSelected: Bool
+    let onToggle: () -> Void
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                Button {
+                    onToggle()
+                } label: {
+                    Text("즐겨찾기")
+                        .applyCertiFont(.caption_semibold_12)
+                        .padding(.horizontal, 12)
+                        .padding(.vertical, 6)
+                        .foregroundStyle(isSelected ? .mainblue : .grayscale400)
+                        .background(
+                            Capsule()
+                                .stroke(isSelected ? .mainblue : .grayscale100, lineWidth: 1)
+                                        )
+                }
+            }
+            .padding(.leading, 23)
+            .padding(.vertical, 12)
+        }
+    }
+}
