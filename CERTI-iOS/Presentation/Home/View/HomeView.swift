aa586f01a3c760b4998c919e5f40e3dfaaf7c37d
OneTen19 <khy000426@naver.com>
2025-07-08 21:28:53 +0900

    [Style] #22 - certiLogo

--- CERTI-iOS/Presentation/Home/View/HomeView.swift
+++ CERTI-iOS/Presentation/Home/View/HomeView.swift
@@ -10,12 +10,51 @@ import SwiftUI
 struct HomeView: View {
     @EnvironmentObject var homeCoordinator: HomeCoordinator
 
+    let username = "김서티22"
+    
     var body: some View {
-        VStack {
-            Text("홈 화면")
-            Button("디테일 보기") {
-                homeCoordinator.push(next: .detail)
+        VStack(alignment: .leading, spacing: 0) {
+            certiLogo
+            ScrollView(.vertical) {
+                VStack(alignment: .leading, spacing: 0) {
+                    HStack(alignment: .center, spacing: 0) {
+                        Text("안녕하세요, ")
+                        Text.trimmedUsername(username)
+                        Text("님!")
+                    }
+                    .frame(height: 26)
+                    .applyCertiFont(.sub_bold_20)
+                    .foregroundStyle(.grayscale600)
+                    .padding(.bottom, 24)
+                    
+                    HStack(alignment: .center, spacing: 0) {
+                        Image(.imageProfileP)
+                        Text.trimmedUsername(username)
+
+                    }
+                }
             }
         }
+        .padding(.horizontal, 20)
+
     }
+}
+
+extension HomeView {
+    private var certiLogo: some View {
+        HStack(alignment: .center, spacing: 0) {
+            Image(.imageLogoBlack)
+            Spacer()
+        }
+        .frame(height: 65)
+        .background(.red)
+    }
+    
+    
+    
+}
+
+#Preview {
+    HomeView()
+        .environmentObject(HomeCoordinator())
 }
