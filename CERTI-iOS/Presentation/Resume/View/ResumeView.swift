40b336bfb87f8a87c740fd602fc725bef3b5cb86
sangyup <sangyeobi712@gmail.com>
2025-07-11 04:17:31 +0900

    [Chore] #33 - 이력서 뷰 코드수정
    extension ResumeView, 띄워쓰기 등

--- CERTI-iOS/Presentation/Resume/View/ResumeView.swift
+++ CERTI-iOS/Presentation/Resume/View/ResumeView.swift
@@ -42,22 +42,26 @@ struct ResumeView: View {
     }
 }
 
-private var ResumeTopMainLogoView: some View {
+extension ResumeView {
+    private var ResumeTopMainLogoView: some View {
         HStack{
             Image(.imageLogoBlack)
+            
             Spacer()
         }
         .padding(.bottom, 36)
         .padding(.horizontal, 20)
     }
-
-private var ResumeProfileView: some View {
+    
+    private var ResumeProfileView: some View {
         HStack{
             Image(.imageProfilePdf)
+            
             VStack(alignment: .leading, spacing: 12){
                 Text("희망직무")
                     .applyCertiFont(.body_semibold_16)
                     .foregroundStyle(.grayscale600)
+                
                 Text("IT/인터넷·경영/사무·경영/사무")
                     .applyCertiFont(.caption_regular_14)
                     .foregroundStyle(.mainblue)
@@ -67,66 +71,70 @@ private var ResumeProfileView: some View {
         .padding(.bottom, 36)
         .padding(.horizontal, 20)
     }
-
-private struct ResumeMyCertificateView: View {
-    let rows = [
-        GridItem(.fixed(100))
-    ]
-    @Binding var isPresented: Bool
     
-    let CertificatedDummy: [CertificatedModel] = CertificatedModel.dummy()
+    private struct ResumeMyCertificateView: View {
+        @Binding var isPresented: Bool
+        @EnvironmentObject var resumeCoordinator: ResumeCoordinator
 
-    var body: some View {
-        VStack{
-            HStack{
-                Text("취득한 자격증")
-                    .applyCertiFont(.sub_semibold_20)
-                    .foregroundStyle(.grayscale600)
-                Spacer()
-                Button {
-                    print("취득한 자격증 편집")
-                } label: {
-                    Image(.iconArrowright36)
-                }
-            }
-            .padding(.horizontal, 20)
-            
-            // 취득한 자격증이 없을 때
-            //            Image(.imageEmpty)
-            //                .padding(.top, 60)
-            //
-            //            Text("취득한 자격증이 없습니다.")
-            //                .applyCertiFont(.caption_regular_14)
-            //                .foregroundStyle(.grayscale400)
-            //                .padding(.bottom, 60)
-            //
-
-            ScrollView(.horizontal) {
-                LazyHGrid(rows: rows, spacing: 12) {
-                    ForEach(CertificatedDummy) { dummy in
-                        ResumeMyCertificateItems(isPresented: $isPresented, cardImage: dummy.cardFrontImageUrl, name: dummy.name, date: dummy.createdAt, certiTag: dummy.tag)
+        let rows = [
+            GridItem(.fixed(100))
+        ]
+        
+        let CertificatedDummy: [CertificatedModel] = CertificatedModel.dummy()
+        
+        var body: some View {
+            VStack{
+                HStack{
+                    Text("취득한 자격증")
+                        .applyCertiFont(.sub_semibold_20)
+                        .foregroundStyle(.grayscale600)
+                    
+                    Spacer()
+                    
+                    Button {
+                        resumeCoordinator.push(next: .myCertificateEdit)
+                    } label: {
+                        Image(.iconArrowright36)
                     }
                 }
-                .padding(.leading, 20)
+                .padding(.horizontal, 20)
+                
+                // 취득한 자격증이 없을 때
+                //            Image(.imageEmpty)
+                //                .padding(.top, 60)
+                //
+                //            Text("취득한 자격증이 없습니다.")
+                //                .applyCertiFont(.caption_regular_14)
+                //                .foregroundStyle(.grayscale400)
+                //                .padding(.bottom, 60)
+                //
+                
+                ScrollView(.horizontal) {
+                    LazyHGrid(rows: rows, spacing: 12) {
+                        ForEach(CertificatedDummy) { dummy in
+                            ResumeMyCertificateItems(isPresented: $isPresented, cardImage: dummy.cardFrontImageUrl, name: dummy.name, date: dummy.createdAt, certiTag: dummy.tag)
+                        }
+                    }
+                    .padding(.leading, 20)
+                }
+                .scrollIndicators(.hidden)
+                
+                Image(.resumeLine)
+                    .padding(.top, 36)
+                    .padding(.bottom, 36)
             }
-            .scrollIndicators(.hidden)
-            
-            Image(.resumeLine)
-                .padding(.top, 36)
-                .padding(.bottom, 36)
         }
     }
-}
-
-struct ResumeMyCertificateItems: View {
-    @Binding var isPresented: Bool
     
-    var cardImage: String = ""
-    var name: String = ""
-    var date: String = ""
-    var certiTag: [String]
-
-    var body: some View {
+    struct ResumeMyCertificateItems: View {
+        @Binding var isPresented: Bool
+        
+        var cardImage: String = ""
+        var name: String = ""
+        var date: String = ""
+        var certiTag: [String]
+        
+        var body: some View {
             ZStack(alignment: .top) {
                 Image(cardImage)
                     .resizable()
@@ -152,64 +160,68 @@ struct ResumeMyCertificateItems: View {
                 }
             }
             .onTapGesture {
-                    isPresented = true
+                isPresented = true
             }
         }
     }
-
-private var ResumeMyCareerView: some View {
-    VStack{
-        HStack{
-            Text("경력사항")
-                .applyCertiFont(.sub_semibold_20)
-                .foregroundStyle(.grayscale600)
-            Spacer()
-            Button {
-                //누르면 편집 뷰
-            } label: {
-                Image(.iconArrowright36)
+    
+    private var ResumeMyCareerView: some View {
+        VStack{
+            HStack{
+                Text("경력사항")
+                    .applyCertiFont(.sub_semibold_20)
+                    .foregroundStyle(.grayscale600)
+                
+                Spacer()
+                
+                Button {
+                    //누르면 편집 뷰
+                } label: {
+                    Image(.iconArrowright36)
+                }
             }
+            .padding(.horizontal, 20)
+            
+            Image(.imageEmpty)
+                .padding(.top, 60)
+            
+            Text("경력사항을 추가해보세요!")
+                .applyCertiFont(.caption_regular_14)
+                .foregroundStyle(.grayscale400)
+                .padding(.bottom, 60)
+            
+            Image(.resumeLine)
+                .padding(.bottom, 36)
         }
-        .padding(.horizontal, 20)
-        
-        Image(.imageEmpty)
-            .padding(.top, 60)
-        
-        Text("경력사항을 추가해보세요!")
-            .applyCertiFont(.caption_regular_14)
-            .foregroundStyle(.grayscale400)
-            .padding(.bottom, 60)
-        
-        Image(.resumeLine)
-            .padding(.bottom, 36)
+    }
+    
+    private var ResumeMyExtracurricularActivityView: some View {
+        VStack{
+            HStack{
+                Text("대내외 활동")
+                    .applyCertiFont(.sub_semibold_20)
+                    .foregroundStyle(.grayscale600)
+                
+                Spacer()
+                
+                Button {
+                    //누르면 편집
+                } label: {
+                    Image(.iconArrowright36)
+                }
+            }
+            .padding(.horizontal, 20)
+            
+            Image(.imageEmpty)
+                .padding(.top, 60)
+            
+            Text("대내외 활동을 추가해보세요!")
+                .applyCertiFont(.caption_regular_14)
+                .foregroundStyle(.grayscale400)
+                .padding(.bottom, 60)
+        }
     }
 }
-
-private var ResumeMyExtracurricularActivityView: some View {
-    VStack{
-        HStack{
-            Text("대내외 활동")
-                .applyCertiFont(.sub_semibold_20)
-                .foregroundStyle(.grayscale600)
-            Spacer()
-            Button {
-                //누르면 편집
-            } label: {
-                Image(.iconArrowright36)
-            }
-        }
-        .padding(.horizontal, 20)
-        
-        Image(.imageEmpty)
-            .padding(.top, 60)
-        
-        Text("대내외 활동을 추가해보세요!")
-            .applyCertiFont(.caption_regular_14)
-            .foregroundStyle(.grayscale400)
-            .padding(.bottom, 60)
-    }
-}
-
 #Preview {
     ResumeView()
 }
