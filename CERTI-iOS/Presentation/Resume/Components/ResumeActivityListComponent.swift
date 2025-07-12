5c4d3c60815cf1d54ce483c04738ef176499b664
sangyup <sangyeobi712@gmail.com>
2025-07-13 02:02:56 +0900

    [Chore] #48 - ResumeActivityListComponent 수정

--- CERTI-iOS/Presentation/Resume/Components/ResumeActivityListComponent.swift
+++ CERTI-iOS/Presentation/Resume/Components/ResumeActivityListComponent.swift
@@ -7,45 +7,44 @@
 
 import SwiftUI
 
-struct ResumeLoadMapComponent: View {
+struct ResumeActivityListComponent: View {
     let model: ResumeModel
     
     var body: some View {
         HStack(alignment: .center, spacing: 0) {
-            VStack(spacing: 0) {
+            VStack(alignment: .leading, spacing: 0) {
                 Text("\(model.startAt) ~ \(model.endAt)")
                     .applyCertiFont(.caption_regular_12)
                     .foregroundStyle(.grayscale500)
-                    .frame(width: 104, height: 18)
+                    .frame(height: 18)
                 
-                Text(model.place)
+                Text.trimmedResumeInfo(model.place, 10)
                     .applyCertiFont(.caption_regular_12)
                     .foregroundStyle(.grayscale500)
-                    .frame(width: 105, height: 18)
+                    .frame(height: 18)
                     .padding(.top, 12)
             }
-            .frame(width: 104, height: 48)
+            .frame(height: 48)
 
             Spacer()
             
-            VStack(spacing: 0) {
-                Text(model.name)
+            VStack(alignment: .leading, spacing: 0) {
+                Text.trimmedResumeInfo(model.name, 9)
                     .applyCertiFont(.body_semibold_16)
                     .foregroundStyle(.grayscale600)
-                    .frame(width: 137, height: 22)
+                    .frame(height: 22)
                 
-                Text(model.discription)
+                Text.trimmedResumeInfo(model.discription, 15)
                     .applyCertiFont(.caption_regular_12)
                     .foregroundStyle(.grayscale600)
                     .lineLimit(1)
-                    .frame(width: 138, height: 18)
+                    .frame(height: 18)
                     .padding(.top, 10)
             }
-            .frame(width: 137, height: 50)
         }
     }
 }
 
 #Preview {
-    ResumeLoadMapComponent(model: ResumeModel.myCareerDummy().first!)
+    ResumeActivityListComponent(model: ResumeModel.myCareerDummy().first!)
 }
