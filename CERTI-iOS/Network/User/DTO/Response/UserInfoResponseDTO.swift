fa43b60efc51b328082fa84a8d0cc9d8cf3069a0
OneTen19 <khy000426@naver.com>
2025-07-15 02:46:10 +0900

    [Feat] #59 - UserInfoResponseDTO

---
+++ CERTI-iOS/Network/User/DTO/Response/UserInfoResponseDTO.swift
@@ -0,0 +1,17 @@
+//
+//  UserInfoResponseDTO.swift
+//  CERTI-iOS
+//
+//  Created by OneTen on 7/15/25.
+//
+
+import Foundation
+
+typealias UserInfoResponseDTO = BaseResponseDTO<UserInfoData>
+
+struct UserInfoData: Decodable {
+    let name: String
+    let university: String
+    let major: String
+    let percentage: Int
+}
