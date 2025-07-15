3afa454e9d66c3790d6915d277e457c799e8fc04
OneTen19 <khy000426@naver.com>
2025-07-16 03:51:38 +0900

    [Feat] #71 - PreCertificationInfoResponseDTO

---
+++ CERTI-iOS/Network/Certification/DTO/Response/CertificationInfoResponseDTO.swift
@@ -0,0 +1,8 @@
+//
+//  CertificationInfoResponseDTO.swift
+//  CERTI-iOS
+//
+//  Created by OneTen on 7/16/25.
+//
+
+import Foundation
---
+++ CERTI-iOS/Network/Home/DTO/Response/PreCertificationInfoResponseDTO.swift
@@ -0,0 +1,10 @@
+//
+//  PreCertificationInfoResponseDTO.swift
+//  CERTI-iOS
+//
+//  Created by OneTen on 7/16/25.
+//
+
+import Foundation
+
+typealias PreCertificationInfoResponseDTO = BaseResponseDTO<CertificationInfoResponseDTO>
