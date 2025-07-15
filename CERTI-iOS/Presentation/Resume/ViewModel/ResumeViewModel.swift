2744051a9a47a8a46af546ce5b62a6b48f82bb35
sangyup <sangyeobi712@gmail.com>
2025-07-15 16:15:56 +0900

    [Network] #63 - getJobList 메서드 생성

--- CERTI-iOS/Presentation/Resume/ViewModel/ResumeViewModel.swift
+++ CERTI-iOS/Presentation/Resume/ViewModel/ResumeViewModel.swift
@@ -7,11 +7,16 @@
 
 import Foundation
 
+import os
+
+@MainActor
 final class ResumeViewModel: ObservableObject {
     @Published var careerDummy: [ResumeModel] = ResumeModel.myCareerDummy()
     @Published var myExtracurricularActivityModelDummy: [ResumeModel] = ResumeModel.myExtracurricularActivityDummy()
     @Published var certificatedDummy: [CertificatedModel] = CertificatedModel.dummy()
     @Published var myJobListDummy: [JobListModel] = JobListModel.myJobListDummy()
+    @Published var jobList: [String] = []
+
 
     @Published var isPeriodFilled: Bool = false
     @Published var resumeModel = ResumeModel(
@@ -24,5 +29,32 @@ final class ResumeViewModel: ObservableObject {
     
     var isWriteButtonEnabled: Bool {
         !resumeModel.name.isBlank && !resumeModel.place.isBlank && !resumeModel.discription.isBlank && isPeriodFilled
+    }
+    
+    private let jobService = NetworkService.shared.jobService
+    private let logger = Logger(subsystem: Bundle.main.bundleIdentifier ?? "CERTI", category: "Job")
+    
+    
+}
+
+// MARK: - Network
+
+extension ResumeViewModel {
+    func getJobList() async {
+        let result = await jobService.getFetchJob()
+        
+        switch result {
+        case .success(let response):
+            guard let data = response.data else {
+                logger.error("❌ getFetchJob: No data received")
+                return
+            }
+            
+            self.jobList = data.jobList
+            logger.debug("✅ getUnivList success: \(data.jobList)")
+            
+        case .failure(let error):
+            logger.error("getJobList failed: \(error.localizedDescription)")
+        }
     }
 }
