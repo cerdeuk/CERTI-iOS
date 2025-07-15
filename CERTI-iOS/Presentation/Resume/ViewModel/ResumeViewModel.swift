2ede339be1c043146166c766b7aa17f7b718b163
sangyup <sangyeobi712@gmail.com>
2025-07-15 17:06:52 +0900

    [Chore[ #63 - ResumeViewModel 코드 정렬

--- CERTI-iOS/Presentation/Resume/ViewModel/ResumeViewModel.swift
+++ CERTI-iOS/Presentation/Resume/ViewModel/ResumeViewModel.swift
@@ -14,10 +14,7 @@ final class ResumeViewModel: ObservableObject {
     @Published var careerDummy: [ResumeModel] = ResumeModel.myCareerDummy()
     @Published var myExtracurricularActivityModelDummy: [ResumeModel] = ResumeModel.myExtracurricularActivityDummy()
     @Published var certificatedDummy: [CertificatedModel] = CertificatedModel.dummy()
-    @Published var myJobListDummy: [JobListModel] = JobListModel.myJobListDummy()
     @Published var jobList: [String] = []
-
-
     @Published var isPeriodFilled: Bool = false
     @Published var resumeModel = ResumeModel(
         startAt: "",
@@ -26,15 +23,11 @@ final class ResumeViewModel: ObservableObject {
         place: "",
         discription: ""
     )
-    
     var isWriteButtonEnabled: Bool {
         !resumeModel.name.isBlank && !resumeModel.place.isBlank && !resumeModel.discription.isBlank && isPeriodFilled
     }
-    
     private let jobService = NetworkService.shared.jobService
     private let logger = Logger(subsystem: Bundle.main.bundleIdentifier ?? "CERTI", category: "Job")
-    
-    
 }
 
 // MARK: - Network
