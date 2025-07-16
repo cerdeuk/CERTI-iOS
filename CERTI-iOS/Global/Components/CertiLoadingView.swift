4cbc88c3e7364b77f6da681fa0c322dbba60a78f
OneTen19 <khy000426@naver.com>
2025-07-16 21:05:54 +0900

    [Add] #75 - CertiLoadingView

---
+++ CERTI-iOS/Global/Components/CertiLoadingView.swift
@@ -0,0 +1,18 @@
+//
+//  CertiLoadingView.swift
+//  CERTI-iOS
+//
+//  Created by OneTen on 7/16/25.
+//
+
+import SwiftUI
+
+struct CertiLoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CertiLoadingView()
+}
