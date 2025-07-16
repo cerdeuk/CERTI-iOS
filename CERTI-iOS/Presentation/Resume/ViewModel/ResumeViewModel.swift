68a7fc49b9cf1c756f98a7283225f75760d95f3a
sangyup <sangyeobi712@gmail.com>
2025-07-16 21:06:20 +0900

    [Network] #77 - ResumeViewModel 메서드 추가

--- CERTI-iOS/Presentation/Resume/ViewModel/ResumeViewModel.swift
+++ CERTI-iOS/Presentation/Resume/ViewModel/ResumeViewModel.swift
@@ -15,6 +15,7 @@ final class ResumeViewModel: ObservableObject {
     @Published var myExtracurricularActivityModelDummy: [ResumeModel] = ResumeModel.myExtracurricularActivityDummy()
     @Published var certificatedDummy: [CertificatedModel] = CertificatedModel.dummy()
     @Published var jobList: [String] = []
+    @Published var acquisitionList: [CertificatedModel] = []
     @Published var isPeriodFilled: Bool = false
     @Published var resumeModel = ResumeModel(
         startAt: "",
@@ -27,6 +28,7 @@ final class ResumeViewModel: ObservableObject {
         !resumeModel.name.isBlank && !resumeModel.place.isBlank && !resumeModel.discription.isBlank && isPeriodFilled
     }
     private let jobService = NetworkService.shared.jobService
+    private let acquisitionService = NetworkService.shared.acquisitionService
     private let logger = Logger(subsystem: Bundle.main.bundleIdentifier ?? "CERTI", category: "Job")
 }
 
@@ -48,6 +50,24 @@ extension ResumeViewModel {
             
         case .failure(let error):
             logger.error("getJobList failed: \(error.localizedDescription)")
+        }
+    }
+    
+    func getAcquisitionList() async {
+        let result = await acquisitionService.fetchAcquisitionList()
+        
+        switch result {
+        case .success(let response):
+            guard let data = response.data else {
+                logger.error("❌ getAcquisitionList: No data received")
+                return
+            }
+            
+            self.acquisitionList = data.getAcquisitionResponses
+            logger.debug("✅ getAcquisitionList success: \(data.getAcquisitionResponses)")
+            
+        case .failure(let error):
+            logger.error("getAcquisitionList failed: \(error.localizedDescription)")
         }
     }
 }
