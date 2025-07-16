38fdac0ea903b9b961878ed4fc54d2abf34b61b7
sangyup <sangyeobi712@gmail.com>
2025-07-17 08:31:10 +0900

    [Network] #89 - AcquisitionDetailResponseDTO

---
+++ CERTI-iOS/Network/Acquisition/DTO/Response/AcquisitionDetailResponseDTO.swift
@@ -0,0 +1,15 @@
+//
+//  AcquisitionDetailResponseDTO.swift
+//  CERTI-iOS
+//
+//  Created by 이상엽 on 7/17/25.
+//
+
+import Foundation
+
+//typealias AcquisitionDetailResponseDTO = BaseResponseDTO<AcquisitionDetailData>
+typealias AcquisitionDetailResponseDTO = BaseResponseDTO<CertificatedModel>
+
+struct AcquisitionDetailData: Decodable {
+    let getAcquisitionResponses: CertificatedModel
+}
