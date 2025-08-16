76e5feabcf4c56bca42d1a47a988478b99e9bd1c
sangyup <sangyeobi712@gmail.com>
2025-08-16 13:25:11 +0900

    [Refactor] #137 - Domain Layer Entity 정의

---
+++ CERTI-iOS/Presentation/Resume/Domain/Entity/ResumeActivity.swift
@@ -0,0 +1,17 @@
+//
+//  ResumeActivity.swift
+//  CERTI-iOS
+//
+//  Created by 이상엽 on 8/16/25.
+//
+
+import Foundation
+
+struct ResumeActivity {
+    let id: Int
+    let startAt: String
+    let endAt: String
+    let name: String
+    let place: String
+    let description: String
+}
---
+++ CERTI-iOS/Presentation/Resume/Domain/Entity/ResumeCareer.swift
@@ -0,0 +1,17 @@
+//
+//  ResumeCareer.swift
+//  CERTI-iOS
+//
+//  Created by 이상엽 on 8/16/25.
+//
+
+import Foundation
+
+struct ResumeCareer {
+    let id: Int
+    let startAt: String
+    let endAt: String
+    let name: String
+    let place: String
+    let description: String
+}
