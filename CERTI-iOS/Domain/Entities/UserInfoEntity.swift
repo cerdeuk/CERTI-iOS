908518483e019e6c454715771998b459e3787e99
OneTen19 <khy000426@naver.com>
2025-08-22 02:51:55 +0900

    [Feat] #140 - add UserInfoEntity

---
+++ CERTI-iOS/Domain/Entities/UserInfoEntity.swift
@@ -0,0 +1,15 @@
+//
+//  UserInfoEntity.swift
+//  CERTI-iOS
+//
+//  Created by OneTen on 8/22/25.
+//
+
+import Foundation
+
+struct UserInfoEntity {
+    let name: String
+    let university: String
+    let major: String
+    let percentage: Int
+}
