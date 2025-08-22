a05859c5594e6a41f78808d8a885c93a6a58e69a
sangyup <sangyeobi712@gmail.com>
2025-08-23 01:59:48 +0900

    [Refactor] #146 - Add, Delete AcquisitionUseCase 생성

--- CERTI-iOS/Domain/UseCases/Acquisition/AddAcquisitionUseCase.swift
+++ CERTI-iOS/Domain/UseCases/Acquisition/AddAcquisitionUseCase.swift
@@ -8,5 +8,18 @@
 import Foundation
 
  protocol AddAcquisitionUseCase {
-    func execute(certificationId: Int) async -> Result<Bool, NetworkError>
+    func execute(certificationId: Int) async -> Result<BaseResponseDTO<Bool>, NetworkError>
+}
+
+final class DefaultAddAcquisitionUseCase: AddAcquisitionUseCase {
+    private let repository: AcquisitionRepository
+    
+    init(repository: AcquisitionRepository) {
+        self.repository = repository
+    }
+    
+    func execute(certificationId: Int) async -> Result<BaseResponseDTO<Bool>, NetworkError> {
+        let result = await repository.addAcquisition(certificationId: certificationId)
+        return await repository.addAcquisition(certificationId: certificationId)
+    }
 }
