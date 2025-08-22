4d57b6a3dafbe565097c63a036032ad8833d1cfb
OneTen19 <khy000426@naver.com>
2025-08-23 01:22:26 +0900

    [Feat] #140 - FetchUnivListUsecase

---
+++ CERTI-iOS/Domain/UseCases/FetchUnivListUsecase.swift
@@ -0,0 +1,26 @@
+//
+//  FetchUnivListUsecase.swift
+//  CERTI-iOS
+//
+//  Created by OneTen on 8/23/25.
+//
+
+import Foundation
+
+protocol FetchUnivListUsecase {
+    func execute(keyword: String, preSignUpToken: String) async -> Result<UniversityListEntity, NetworkError>
+}
+
+final class DefaultFetchUnivListUsecase: FetchUnivListUsecase {
+    
+    private let repository: OnboardingRepository
+
+    init(repository: OnboardingRepository) {
+        self.repository = repository
+    }
+
+    func execute(keyword: String, preSignUpToken: String) async -> Result<UniversityListEntity, NetworkError> {
+        await repository.getSearchUniv(keyword: keyword, preSignUpToken: preSignUpToken)
+    }
+    
+}
