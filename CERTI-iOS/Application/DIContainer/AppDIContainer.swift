e4ed50b0aacc09ef28cc9761d3fafc25ce39eecc
OneTen19 <khy000426@naver.com>
2025-08-25 14:53:53 +0900

    [Chore] #140 - Conflict 해결

--- CERTI-iOS/Application/DIContainer/AppDIContainer.swift
+++ CERTI-iOS/Application/DIContainer/AppDIContainer.swift
@@ -89,20 +89,14 @@ extension AppDIContainer {
     
     func makeFetchMajorListUseCase() -> FetchMajorListUseCase {
         return DefaultFetchMajorListUseCase(repository: onboardingRepository)
-        return DefaultGetFavoriteCertificationUseCase(repository: homeRepositoryInstance)
-    }
-    
-    // 이거를 어디서 써줘야할지.. 익스텐션 맨 위에? 흑..
-    private var jobRepositoryInstance: JobRepository {
-        return DefaultJobRepository(service: makeJobService())
     }
     
     func makeFetchJobUseCase() -> FetchJobUseCase {
-        return DefaultFetchJobUseCase(repository: jobRepositoryInstance)
+        return DefaultFetchJobUseCase(repository: jobRepository)
     }
     
     func makeEditJobUseCase() -> EditJobUseCase {
-        return DefaultEditJobUseCase(repository: jobRepositoryInstance)
+        return DefaultEditJobUseCase(repository: jobRepository)
     }
 }
 
