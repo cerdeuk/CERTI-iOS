8e1b384f65fe9d460939d2e2f6bc3116efbcb0bf
sangyup <sangyeobi712@gmail.com>
2025-08-26 15:51:51 +0900

    [Refactor] #149 - AddAcquisition 수정
    반환타입 수정

--- CERTI-iOS/Domain/UseCases/Acquisition/AddAcquisitionUseCase.swift
+++ CERTI-iOS/Domain/UseCases/Acquisition/AddAcquisitionUseCase.swift
@@ -8,7 +8,7 @@
 import Foundation
 
  protocol AddAcquisitionUseCase {
-    func execute(certificationId: Int) async -> Result<Void, NetworkError>
+    func execute(certificationId: Int) async -> Result<Bool, NetworkError>
 }
 
 final class DefaultAddAcquisitionUseCase: AddAcquisitionUseCase {
@@ -18,7 +18,8 @@ final class DefaultAddAcquisitionUseCase: AddAcquisitionUseCase {
         self.repository = repository
     }
     
-    func execute(certificationId: Int) async -> Result<Void, NetworkError> {
-        return await repository.addAcquisition(certificationId: certificationId)
+    func execute(certificationId: Int) async -> Result<Bool, NetworkError> {
+        let result = await repository.addAcquisition(certificationId: certificationId)
+        return result.map { $0.data ?? false }
     }
 }
