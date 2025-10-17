848f4976646724329831abc60d951f6322282cd8
OneTen19 <khy000426@naver.com>
2025-10-17 15:41:47 +0900

    [Add] #179 - OnboardingNickNameView

---
+++ CERTI-iOS/Presentation/Onboarding/View/OnboardingNickNameView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingNickNameView.swift
+//  CERTI-iOS
+//
+//  Created by OneTen on 10/17/25.
+//
+
+import SwiftUI
+
+struct OnboardingNickNameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingNickNameView()
+}
