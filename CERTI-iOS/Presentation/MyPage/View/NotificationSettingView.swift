0f5483c7caabd56daa26856ba912fbd8ea02a76a
OneTen19 <khy000426@naver.com>
2025-11-02 00:44:50 +0900

    [Style] #181 - 동의 토스트 메세지

--- CERTI-iOS/Presentation/MyPage/View/NotificationSettingView.swift
+++ CERTI-iOS/Presentation/MyPage/View/NotificationSettingView.swift
@@ -10,21 +10,52 @@ import SwiftUI
 struct NotificationSettingView: View {
     @ObservedObject var viewModel: MyPageViewModel
     
+    // TODO: - Userdefault로 관리해야 하나? 아니면 앱 내부 설정을 불러올 수 있는 기능이 있나? 좀 찾아보기
     @State private var agreeState: Bool = false
+    @State private var showConfirmationAlert = false
+    @State private var showToastMessage = false
+    
+    private var dateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy.MM.dd"
+        formatter.locale = Locale(identifier: "ko_KR")
+        return formatter
+    }
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 0) {
-            headerView
+        ZStack(alignment: .bottom) {
+            VStack(alignment: .leading, spacing: 0) {
+                headerView
+                
+                agreeToggleButton
+                    .padding(.top, 20)
+                    .padding(.horizontal, 20)
+                    .padding(.bottom, 12)
+                
+                Text("마케팅을 위한 이벤트, 공지사항 등의 소식 알림 받기")
+                    .applyCertiFont(.caption_regular_14)
+                    .foregroundStyle(.grayscale400)
+                    .frame(height: 20)
+                    .padding(.horizontal, 20)
+                    .padding(.bottom, 24)
+                
+                checkBoxSection
+                
+                Spacer()
+            }
             
-            agreeToggleButton
-                .padding(.top, 20)
-                .padding(.horizontal, 20)
-                .padding(.bottom, 12)
-            
-            
-            
-            Spacer()
+            if showToastMessage {
+                ToastMessageView(
+                    message: "광고성 정보를 받는 것에 동의했어요. \(dateFormatter.string(from: Date()))",
+                    style: .agreeMarketing
+                )
+                .transition(.move(edge: .bottom).combined(with: .opacity))
+                .zIndex(1)
+                .padding(.bottom, 36)
+            }
         }
+        .animation(.spring, value: showToastMessage)
+        
     }
 }
 
@@ -57,23 +88,89 @@ extension NotificationSettingView {
     
     @ViewBuilder
     private var agreeToggleButton: some View {
-        Toggle(isOn: $agreeState) {
-            HStack(alignment: .center, spacing: 0) {
-                Text("광고성 정보 수신 동의")
-                    .applyCertiFont(.body_semibold_16)
-                    .foregroundStyle(.black)
-                    .padding(.trailing, 4)
-                
-                Button {
-                    // 도움말 팝업
-                } label: {
-                    Image(.iconQuestion24)
+        let toggleBinding = Binding<Bool>(
+                get: {
+                    agreeState
+                },
+                set: { newValue in
+                    if newValue == true {
+//                        showConfirmationAlert = true
+                        agreeState = true
+                        
+                        Task {
+                            showToastMessage = true
+                            try await Task.sleep(for: .seconds(2))
+                            showToastMessage = false
+                        }
+                        
+                    } else {
+                        agreeState = false
+                        showToastMessage = false
+                    }
+                }
+            )
+            
+            Toggle(isOn: toggleBinding) {
+                HStack(alignment: .center, spacing: 0) {
+                    Text("광고성 정보 수신 동의")
+                        .applyCertiFont(.body_semibold_16)
+                        .foregroundStyle(.black)
+                        .padding(.trailing, 4)
+                 
+                    Button {
+                        // 도움말 팝업
+                    } label: {
+                        Image(.iconQuestion24)
+                    }
                 }
             }
-        }
-        .tint(.purpleblue)
-        .frame(height: 24)
+            .tint(.purpleblue)
+            .frame(height: 24)
     }
     
+    @ViewBuilder
+    private var checkBoxSection: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            
+            HStack(alignment: .center, spacing: 0) {
+                Image(systemName: agreeState ? "checkmark.square.fill" : "square")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 20)
+                    .foregroundStyle(agreeState ? .grayscale500 : .grayscale400)
+                    .padding(.trailing, 12)
+                
+                Text("(선택)")
+                    .applyCertiFont(.body_regular_16)
+                    .foregroundStyle(.grayscale400)
+                    .padding(.trailing, 4)
+                
+                Text("서티 개인정보 수집 및 이용 동의")
+                    .applyCertiFont(.body_semibold_16)
+                    .foregroundStyle(.black)
+                
+                Spacer()
+                
+                Button {
+                    // 노션이동
+                } label: {
+                    Image(.iconArrowright24)
+                }
+                
+            }
+            .frame(height: 24)
+            .padding(.horizontal, 20)
+            .padding(.bottom, 12)
+            .padding(.top, 16)
+            
+            Text("이벤트 소식 알림을 받으려면 먼저 개인정보 수집 및 이용에 동의해주세요.")
+                .applyCertiFont(.caption_regular_14)
+                .foregroundStyle(.grayscale400)
+                .padding(.horizontal, 20)
+                .padding(.bottom, 16)
+            
+        }
+        .background(.grayscale0)
+    }
     
 }
