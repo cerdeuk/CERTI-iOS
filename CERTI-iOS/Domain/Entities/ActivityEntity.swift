bfd08e79e3b7d7311d6b16955e712e0333331417
sangyup <sangyeobi712@gmail.com>
2026-01-17 16:21:04 +0900

    [Refactor] #184 - Domain 계층 Entity 분리
    career / activity

---
+++ CERTI-iOS/Domain/Entities/ActivityEntity.swift
@@ -0,0 +1,42 @@
+//
+//  ActivityEntity.swift
+//  CERTI-iOS
+//
+//  Created by 이상엽 on 1/17/26.
+//
+
+import Foundation
+
+struct ActivityEntity {
+    let activityId: Int
+    let startAt: String
+    let endAt: String
+    let name: String
+    let place: String
+    let description: String
+}
+
+// MARK: - Func
+
+extension ActivityEntity {
+    func toAddActivityRequestDTO() -> AddActivityRequestDTO {
+        AddActivityRequestDTO(
+            startAt: startAt,
+            endAt: endAt,
+            place: place,
+            name: name,
+            description: description
+        )
+    }
+    
+    func toActivityModel() -> ActivityModel {
+        ActivityModel(
+            activityId: activityId,
+            startAt: startAt,
+            endAt: endAt,
+            name: name,
+            place: place,
+            description: description
+        )
+    }
+}
---
+++ CERTI-iOS/Domain/Entities/CareerEntity.swift
@@ -0,0 +1,52 @@
+//
+//  CareerEntity.swift
+//  CERTI-iOS
+//
+//  Created by 이상엽 on 1/17/26.
+//
+
+import Foundation
+
+struct CareerEntity {
+    let careerId: Int
+    let startAt: String
+    let endAt: String
+    let name: String
+    let place: String
+    let description: String
+}
+
+// MARK: - Func
+
+extension CareerEntity {
+    func toAddCareerRequestDTO() -> AddCareerRequestDTO {
+        AddCareerRequestDTO(
+            startAt: startAt,
+            endAt: endAt,
+            place: place,
+            name: name,
+            description: description
+        )
+    }
+    
+    func toEditCareerRequestDTO() -> EditCareerRequestDTO {
+        EditCareerRequestDTO(
+            startAt: startAt,
+            endAt: endAt,
+            place: place,
+            name: name,
+            description: description
+        )
+    }
+    
+    func toCareerModel() -> CareerModel {
+        CareerModel(
+            careerId: careerId,
+            startAt: startAt,
+            endAt: endAt,
+            name: name,
+            place: place,
+            description: description
+        )
+    }
+}
